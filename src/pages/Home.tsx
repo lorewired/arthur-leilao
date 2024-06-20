ad6deb8986e0d830c6957ef14c4a8780e057da30
= <lorecode12@gmail.com>
2024-06-20 20:51:47 +0000

    refactor: nothing special

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/Container"
+
+const Home = () => {
+    return (
+        <Container>
+            <div>
+                Home
+            </div>
+        </Container>
+    )
+}
+
+export default Home
