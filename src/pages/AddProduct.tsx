e0e90a4454bc69aa61c8909bb42430305d2b5b05
= <lorecode12@gmail.com>
2024-06-20 20:41:55 +0000

    feat: add AddProduct page

---
+++ src/pages/AddProduct.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/Container"
+
+const AddProduct = () => {
+  return (
+    <Container>
+        <div>
+          AddProduct
+        </div>
+    </Container>
+  )
+}
+
+export default AddProduct
