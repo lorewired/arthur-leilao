8d4536f8655272f1c23f22d97b1b2124b99ded94
= <lorecode12@gmail.com>
2024-06-20 20:42:36 +0000

    feat: add Products page

---
+++ src/pages/Products.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/Container"
+
+const Products = () => {
+  return (
+    <Container>
+      <div>
+        Products
+      </div>
+    </Container>
+  )
+}
+
+export default Products
