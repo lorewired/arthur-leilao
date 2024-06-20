9bd88ce6843507283215086e0bf0864b9861a746
= <lorecode12@gmail.com>
2024-06-20 20:42:19 +0000

    feat: add My Bids page

---
+++ src/pages/MyBids.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/Container"
+
+const MyBids = () => {
+  return (
+    <Container>
+      <div>
+        My Bids
+      </div>
+    </Container>
+  )
+}
+
+export default MyBids
