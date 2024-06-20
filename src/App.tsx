ba0293a83be5fe66de1c1c589f38d67593d5a807
= <lorecode12@gmail.com>
2024-06-20 20:48:00 +0000

    build: add dependencies including tailwind files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import NavBar from "./components/NavBar"
+import Container from "./components/Container"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <div className="w-screen flex">
+        <NavBar />
+        <Container>
+          <Outlet />
+        </Container>
+      </div>
+    </div>
   )
 }
 
