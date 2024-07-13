093943ab887125f2e1a74c0bc42af1b5ab783a78
= <lorecode12@gmail.com>
2024-07-13 02:36:03 +0000

    fix not found error

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -25,7 +25,7 @@ const AddProduct = () => {
     if (anyInvalidCredential) return;
 
     try {
-      const response = await fetch('http://localhost:3000/api/product', {
+      const response = await fetch('https://arthur-leilao-api-production.up.railway.app/api/product', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
